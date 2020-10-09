8de34b1638f3cd410143ffabcd6a8c2c33ebc1dc
Andreas Renggli <andreas.renggli@ziemergroup.com>
2020-10-09 18:06:33 +0200

    Added Prism FrameWork and separated views

---
+++ LogAnalyser/ViewModels/PatternAnalysisViewModel.cs
@@ -0,0 +1,120 @@
+﻿using LogAnalyser.Extension;
+using LogAnalyser.Helper;
+using LogAnalyser.Interfaces;
+using LogAnalyser.IO;
+using Microsoft.Win32;
+using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LogAnalyser.ViewModels
+{
+    public class PatternAnalysisViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public ICommand DataSet1FileCommand { get; set; }
+        public ICommand DataSet2FileCommand { get; set; }
+        public ICommand DataSet3FileCommand { get; set; }
+        public ICommand Analyse12Command { get; set; }
+        public ICommand Analyse23Command { get; set; }
+        public IHistogram Histogram { get; set; } = new Histogram();
+        public IPattern Pattern1 { get; set; }
+        public IPattern Pattern2 { get; set; }
+        public IPattern Pattern3 { get; set; }
+        public ObservableCollection<DataPoint> HistoSet { get; private set; } = new ObservableCollection<DataPoint>();
+        public int BinValue { get; set; }
+        public string Title { get; set; }
+
+        public PatternAnalysisViewModel()
+        {
+            this.DataSet1FileCommand = new BaseCommand(true, DataSet1FileHandler);
+            this.DataSet2FileCommand = new BaseCommand(true, DataSet2FileHandler);
+            this.DataSet3FileCommand = new BaseCommand(true, DataSet3FileHandler);
+            this.Analyse12Command = new BaseCommand(true, Analyse12Handler);
+            this.Analyse23Command = new BaseCommand(true, Analyse23Handler);
+            BinValue = 20;
+            Title = "Test";
+
+        }
+
+        public void Analyse12Handler()
+        {
+            List<double> deltaList = new List<double>();
+            for (int i = 0; i < this.Pattern2.Points.Count; i++)
+            {
+                var p1 = new Point { X = this.Pattern2.Points[i].X, Y = this.Pattern2.Points[i].Y };
+                var p2 = new Point { X = this.Pattern3.Points[i].X, Y = this.Pattern3.Points[i].Y };
+                deltaList.Add((p1 - p2).Length);
+            }
+            var histo = Histogram.Create(deltaList, BinValue);
+            this.HistoSet = Histogram.Points.ToObservableCollection();
+            this.OnPropertyChanged(nameof(this.HistoSet));
+        }
+
+        public void Analyse23Handler()
+        {
+            List<double> deltaList = new List<double>();
+            for (int i = 0; i < this.Pattern2.Points.Count; i++)
+            {
+                var p1 = new Point { X = this.Pattern2.Points[i].X, Y = this.Pattern2.Points[i].Y };
+                var p2 = new Point { X = this.Pattern3.Points[i].X, Y = this.Pattern3.Points[i].Y };
+                deltaList.Add((p1 - p2).Length);
+            }
+            var histo = Histogram.Create(deltaList, BinValue);
+            this.HistoSet = Histogram.Points.ToObservableCollection();
+            this.OnPropertyChanged(nameof(this.HistoSet));
+        }
+
+        public void DataSet1FileHandler()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+  
+                this.Pattern1 = new Pattern(openFileDialog.FileName);
+                this.OnPropertyChanged(nameof(this.Pattern1));
+
+
+                /*{
+                    this.FileName = string.Empty;
+                    MessageBox.Show("Invalid file!");
+                }*/
+            }
+        }
+
+        public void DataSet2FileHandler()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                this.Pattern2 = new Pattern(openFileDialog.FileName);
+                this.OnPropertyChanged(nameof(this.Pattern2));
+            }
+        }
+
+        public void DataSet3FileHandler()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                this.Pattern3 = new Pattern(openFileDialog.FileName);
+                this.OnPropertyChanged(nameof(this.Pattern3));
+            }
+        }
+
+
+
+    }
+}
---
+++ LogAnalyser/Views/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using LogAnalyser.ViewModels;
+using LogAnalyser.Views;
+using Prism.Ioc;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogAnalyser.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IContainerExtension _container;
+        IRegionManager _regionManager;
+        IRegion _region;
+
+        PatternAnalysisView _patternAnalysisView;
+
+        public MainWindow(IContainerExtension container, IRegionManager regionManager)
+        {
+            InitializeComponent();
+
+            _container = container;
+            _regionManager = regionManager;
+
+            this.Loaded += MainWindow_Loaded;
+
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _patternAnalysisView = _container.Resolve<PatternAnalysisView>();
+            
+
+            _region = _regionManager.Regions["ContentRegion"];
+
+            _region.Add(_patternAnalysisView);
+        }
+
+        private void Pattern_Clicked(object sender, RoutedEventArgs e)
+        {
+            //activate view a
+            _region.Activate(_patternAnalysisView);
+        }
+    }
+}
